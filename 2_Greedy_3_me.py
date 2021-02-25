5fc74f7f2e1d1f603ad5df7c5e1a0d4f19c1ddb1
jsubermensch <worldest0628@gmail.com>
2021-02-25 17:55:01 +0900

    #곱하기 혹은 더하기 (2. 그리디 & 구현 20:10)
    예시를 하나씩 들어서 구체적으로 이해하고
    손으로 pseudo code 만들면 쉽게 풀 수 있다.

--- 2_Greedy_3_me.py
+++ 2_Greedy_3_me.py
@@ -1,2 +1,15 @@
 array = list(map(int, input().split()))
-print(array)
+N_array = len(array)
+
+sum = 0
+product = 0
+result = array[0]
+for i in range(N_array-1):
+        sum = result + array[i+1]
+        product = result * array[i+1]
+        if sum > product:
+            result = sum
+        else:
+            result = product
+
+print(result)
