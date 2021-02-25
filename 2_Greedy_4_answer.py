b52d26b7698d3dee619612e3a4789a1c48d821b7
jsubermensch <worldest0628@gmail.com>
2021-02-25 20:34:23 +0900

    #모험가 길드 문제 (27:22)
    w. 좀 더 낱개 단위로 잘라서 풀면 더 쉽게 풀었을 텐데..
    
    s. 낱개 단위로 단개 단개 더 쪼개서 보기.

---
+++ 2_Greedy_4_answer.py
@@ -0,0 +1,17 @@
+data = map(int, input().split())
+sorted_data = sorted(data) #이거 매서드 외워야지.
+
+result = 0
+count = 0
+
+for i in sorted_data:
+    count +=1
+    if i > count:
+        continue
+    else:
+        result+=1
+        count = 0
+
+print(result)
+
+
